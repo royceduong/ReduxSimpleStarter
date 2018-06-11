bbaa8a8730ab7c4b9a7a9466725a694b58e4a59f
Royce Duong <royceduong@Royces-MacBook-Pro.local>
2018-06-11 14:28:04 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCmt5hkrpcG77CMfsh-qg8dX4y0LIoAeAU';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+//App as a CLASS COMPONENT
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+         };
+         this.videoSearch('surfboards');
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            // this.setState({ videos: videos })
+            //ES6 SYNTAX is below.
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            })
+        });
+    }
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={ videoSearch }/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+// App as a FUNCTIONAL COMPONENT
+// const App = () => {
+//     return (
+//         <div>
+//             <SearchBar />
+//         </div>
+//     );
+// }
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
